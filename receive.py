76f2baba659a6098b2a76b6fbd574f9033ee219c
Dyb <rdybkj21@student.aau.dk>
2023-10-22 13:55:48 +0200

    Added timeout

--- receive.py
+++ receive.py
@@ -4,10 +4,13 @@ import spongebobcase
 
 def main():
     radio = apc220.ADC220()
-    res = radio.receive()
-    ReS = spongebobcase.tospongebob(res)
-    radio.transmit(ReS)
-
+    res = radio.receive(timeout = 20)
+    if(""!=res):
+        print("Received something!")
+        ReS = spongebobcase.tospongebob(res)
+        radio.transmit(ReS)
+    else:
+        print("Timed out!")
     print("The end!")
     radio.close()
 
