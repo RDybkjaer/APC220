3848c44d91316efb520d3ead9847cd07f01d4f90
Dyb <rdybkj21@student.aau.dk>
2023-10-22 14:25:30 +0200

    Begun program for balloon

---
+++ balloon.py
@@ -0,0 +1,24 @@
+import APC220 as apc220
+from spongebobcase import tospongebob
+
+
+def main():
+    radio = apc220.ADC220()
+    print("Waiting for sync message!")
+    radio.receive(timeout=None)
+    print("Synced! - beginning cycle")
+    while(1):
+        res = radio.receive(timeout = 10)
+        if(""!=res):
+            print("Received something!")
+            ReS = tospongebob(res)
+            radio.transmit(ReS)
+        else:
+            print("Timed out - Sending telemetry!")
+            radio.transmit("Nothing received!!")
+        print("The end!")
+    radio.close()
+
+
+if __name__ == "__main__":
+    main()
