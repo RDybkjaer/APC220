fffafe5a2ca6996179d0fd6765f326f4efca6514
Dyb <rdybkj21@student.aau.dk>
2023-10-22 17:19:19 +0200

    Deleted PyUSBlinks

--- receive.py
+++ receive.py
@@ -1,14 +1,16 @@
 import APC220 as apc220
-import spongebobcase
-
 
 def main():
+    res: str
+    ReS: str
+    #Initialiserer serialforbindelsen til radioen
     radio = apc220.ADC220()
-    res = radio.treceive(timeout = 20)
+    #
+    res = radio.receive(timeout = 20)
     if(""!=res):
         print("Received something!")
-        ReS = spongebobcase.tospongebob(res)
-        radio.ttransmit(ReS)
+        ReS = res.capitalize()
+        radio.transmit(ReS)
     else:
         print("Timed out!")
     print("The end!")
