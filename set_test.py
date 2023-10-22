e3e5f12621aacc03ed605c246c91e2336def1339
Dyb <rdybkj21@student.aau.dk>
2023-10-22 11:18:41 +0200

    removed space

--- set_test.py
+++ set_test.py
@@ -5,7 +5,7 @@ def main():
     print("Bonjour world")
     # Vi starter initialiseringen af en Seriel forbindelse - Her benyttes pySerial biblioteket
     radio = adc.ADC220()
-    radio.send("Hello world! \n I am a test message")
+    radio.send("Hello world! \nI am a test message")
 
 
 if __name__ == "__main__":
