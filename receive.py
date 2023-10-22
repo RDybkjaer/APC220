7663d8dbefd2cd9f868a426ac6de64399f4d23b4
Dyb <rdybkj21@student.aau.dk>
2023-10-22 17:25:19 +0200

    added comments to receive.py

--- receive.py
+++ receive.py
@@ -1,17 +1,25 @@
 import APC220 as apc220
 
 def main():
-    res: str
-    ReS: str
+    received: str
+    toReturn: str
     #Initialiserer serialforbindelsen til radioen
     radio = apc220.ADC220()
-    #
-    res = radio.receive(timeout = 20)
-    if(""!=res):
-        print("Received something!")
-        ReS = res.capitalize()
-        radio.transmit(ReS)
+
+    #Modtager en besked - timer out hvis den ikke kommer
+    #indenfor 20 sekunder
+    received = radio.receive(timeout = 20)
+    
+    #Hvis 
+    if(""!=received):
+        print("Received: "+str(received))
+        #Capitaliser for at sende noget andet tilbage
+        toReturn = received.capitalize()
+        #Transmitter det capitalized resultat tilbage
+        radio.transmit(toReturn)
     else:
+        #Hvis received == "", er det FORMODENLIGT fordi den timer ud.
+        #TODO: Vi skal lige være sikre på at det passer :P
         print("Timed out!")
     print("The end!")
     radio.close()
