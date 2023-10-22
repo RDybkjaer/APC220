28d2e2cc7eb132b625cbddf4ca7845a5a0eb73c4
Dyb <rdybkj21@student.aau.dk>
2023-10-22 10:59:08 +0200

    Moved receive to ADC220

--- ADC220.py
+++ ADC220.py
@@ -1,40 +1,53 @@
 import serial
 
+
 class ADC220(serial.Serial):
-    def __init__(self):
+    endchar: bytes
+
+    def __init__(self, endchar: bytes = b"!#"):
         super().__init__()
+        self.endchar = endchar
         # Til serial i linux benyttes portene /dev/ttyUSB0
         # "/dev/ttyUSB0"
-        #self.s = serial.Serial()
+        # self.s = serial.Serial()
         self.port = "/dev/ttyUSB0"
-        #Godt nok er parity som standard sat til N, men for god ordens skyld
+        # Godt nok er parity som standard sat til N, men for god ordens skyld
         self.parity = "N"
-        #Her sættes en timeout for, da jeg havde problemer med at den læste for evigt
+        # Her sættes en timeout for, da jeg havde problemer med at den læste for evigt
         self.timeout = 10
         self.baudrate = 9600
         self.rtscts = False
-        #Porten åbnes officielt
+        # Porten åbnes officielt
         self.open()
         print("Serial data: " + str(self))
-        #Test om porten er åben
+        # Test om porten er åben
         sio = self.is_open
         print("Is open: " + str(sio))
-        #Test om porten kan skrives t
+        # Test om porten kan skrives t
         swa = self.writable()
         print("Is writeable: " + str(swa))
-        #Test om porten kan læses fra
+        # Test om porten kan læses fra
         sra = self.readable()
         print("Is readable: " + str(sra))
-    
+
     def send(self, msg):
         print(type(msg))
-        msg = msg.encode('utf-8')
+        msg = msg + self.endchar
+        msg = msg.encode("utf-8")
         print(type(msg))
         print(msg)
-        print("Sent: " +str(msg))
+        print("Sent: " + str(msg))
         j = self.write(msg)
         print("Has written: " + str(j))
-    
-    def io(self) -> bool:
-        sio = self.is_open
-        return sio
+
+    def receive(self) -> bytes:
+        # Her sendes Hello World 5 gange
+        print("Ready 2 read")
+        # Læser 5 bytes
+        r = self.read_until()
+        print(type(r))
+        print(r)
+        b = r.decode("utf-8")
+        print(type(b))
+        print("Read: " + b)
+        return b
